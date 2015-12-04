b9b4baee136c64298f799bb931239503b0e6d77c
capncapsize <daveng-2@student.ltu.se>
2015-12-04 16:39:49 +0100

    Made all crash :( Abando ship

--- ElectroStat/app/src/main/java/info/androidhive/jsonparsing/ServiceHandler.java
+++ ElectroStat/app/src/main/java/info/androidhive/jsonparsing/ServiceHandler.java
@@ -1,25 +1,23 @@
 package info.androidhive.jsonparsing;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ServiceHandler extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_service_handler);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_service_handler, menu);
         return true;
     }
 
@@ -36,11 +34,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, ServiceHandler.class);
-        MainActivity.this.startActivity(intent);
-
     }
 }
---
+++ ElectroStat/app/src/main/java/info/androidhive/jsonparsing/ServiceHandlerFragment.java
@@ -0,0 +1,22 @@
+package info.androidhive.jsonparsing;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ServiceHandlerFragment extends Fragment {
+
+    public ServiceHandlerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_service_handler, container, false);
+    }
+}
