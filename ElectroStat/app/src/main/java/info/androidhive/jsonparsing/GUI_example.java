747f3ff959a826a1df846f795d2475757a5ab760
capncapsize <daveng-2@student.ltu.se>
2015-12-04 16:41:41 +0100

    dunno why this was not cxommited

--- ElectroStat/app/src/main/java/info/androidhive/jsonparsing/GUI_example.java
+++ ElectroStat/app/src/main/java/info/androidhive/jsonparsing/GUI_example.java
@@ -1,18 +1,23 @@
 package info.androidhive.jsonparsing;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GUI_example extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_gui_example);
+
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
